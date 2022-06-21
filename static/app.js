e9190b77cdbb67d4f38162653e1a9cb1000e2916
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2022-06-20 18:45:18 -0700

    Having trouble changing font color on program template page

---
+++ static/app.js
@@ -0,0 +1,28 @@
+// let i = 0;
+// window.onload = function () {
+//     var backgroundImg=[
+//                         "https://images.unsplash.com/photo-1605296867304-46d5465a13f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+//                        "https://images.unsplash.com/photo-1499438075715-fc23ef376ab9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1421&q=80",
+//                        "https://images.unsplash.com/photo-1606846633596-cbec284ed481?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+//                        ]
+
+//     setInterval(changeImage, 5000);
+    
+//     function changeImage() {   
+//     var i = Math.floor((Math.random() * 3));
+
+//     document.body.style.backgroundImage = "url('"+backgroundImg[i]+"')";  
+//     document.body.style.backgroundImage = 
+//     console.log(i)
+    
+//      }
+     
+//    }
+
+
+//    [
+//     "https://images.unsplash.com/photo-1605296867304-46d5465a13f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+//    "https://images.unsplash.com/photo-1571902943202-507ec2618e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1375&q=80",
+//    "https://images.unsplash.com/photo-1544033527-b192daee1f5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+//    "https://images.unsplash.com/photo-1554284126-aa88f22d8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1294&q=80"
+//    ]
